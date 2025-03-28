54a90c81dae6c2d84190d8af7bd3dfe222dcff1a
Patrik Regan <40373909+pregan21@users.noreply.github.com>
2025-03-28 00:38:10 -0400

    main v3
    Updated advance_cycles to check if there re no more instructions. Really bad optimization, however, as it does a 1024 count for loop every cycle. Should only be temp

--- main.cpp
+++ main.cpp
@@ -35,6 +35,7 @@ void Issue();
 void Dispatch();
 void Fetch();
 bool Advance_Cycle();
+bool isEmpty();
 
 instruction dispatch[1024];
 instruction issue[1024];
@@ -52,6 +53,7 @@ int main(int argc, const char * argv[]) {
         Dispatch();
         Fetch();
     } while(Advance_Cycle());
+    
     return 0;
 }
 
@@ -76,10 +78,20 @@ void Fetch() {
 }
 
 bool Advance_Cycle() {
-    if() //If there are no more instructions left, end calculations
+    if(isEmpty()) //If there are no more instructions left, end calculations
         return false;
     else { //If there are more instructions to calculate, increment to next cycle and continue
         cycle++;
         return true;
     }
+}
+
+//Checks if empty. Very bad optimization, needs to be adjusted
+bool isEmpty() {
+    for(int i = 0; i <= 1024; i++) {
+        if (dispatch[i].state != WB && dispatch[i].state != IF) return false;
+        if (issue[i].state != WB && issue[i].state != IF) return false;
+        if (execute[i].state != WB && execute[i].state != IF) return false;
+    }
+    return true;
 }
