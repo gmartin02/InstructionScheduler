2dffabc67f5c73a88ce1d4c00217b57c5cacbdc0
UB <nil>
2025-04-03 04:26:11 -0400

    Program so far finishes with code 0, but still needs to trace its outputs. Everything is otherwise filled out.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <algorithm>
 using namespace std;
 
-const int N = 2; // Max size for queue sizes.
+const int N = 512; // Max size for queue sizes.
 
 int cycle = 0; // Cycle that program is on
 ifstream file("val_trace_gcc.txt");
@@ -25,6 +25,7 @@ enum state_list {
 class instruction {
     public:
     enum state_list state;
+    int special_flag = 0;
     int tag;
     int address;
     int operation;
@@ -67,22 +68,26 @@ void Fetch();
 bool Advance_Cycle();
 bool isEmpty();
 
+// Pointers are useful for a project like this since it allows us to access the data easily anywhere, anytime, between any data structure.
 vector<instruction*> dispatch;
 vector<instruction*> issue;
 vector<instruction*> execute;
 vector<instruction*> fakeROB;
+//instruction* RF_pointer[128];
 
 int main(int argc, const char * argv[]) {
 
     init_RF();
     
     do {
+        /* Debug code
         cout << "----------- Iteration: " << cycle << "-----------" << endl;
         cout << "dispatch" << dispatch.size() << endl;
         cout << "issue" << issue.size() << endl;
         cout << "execute" << execute.size() << endl;
         cout << "fakeROB" << fakeROB.size() << endl;
         cout << endl;
+        */
         
         //cout << "hit before fakeretire" << endl;
         FakeRetire();
@@ -100,6 +105,7 @@ int main(int argc, const char * argv[]) {
     } while(Advance_Cycle());
 
     cout << "Program End" << endl;
+    cout << cycle << endl;
      
     return 0;
 }
@@ -127,7 +133,7 @@ void FakeRetire() {
     }
 }
 
-// Each instruction has an operation value (0, 1, 2) which describe its execution latency, or how many cycles we delay it in the simulator
+// Each instruction has an operation value (0, 1, 2) which describe its execution latency, or how many cycles we delay it in the simulator.
 // Once the instruction passes through its required cycles, we remove it from the execute queue.
 void Execute() {
     if (execute.empty()) return;
@@ -150,9 +156,10 @@ void Execute() {
             continue;
         }
 
+        // We access the destination register (dest_o), convert it to integer with stoi(), plug that number into the RF array, and finally get
+        // the tag by using the second row of the array [<some number>][1];
         if ((*it)->dest_o != "-1") {
-            RF[stoi((*it)->dest_o)][1] = 0; // We access the destination register (dest_o), convert it to integer, plug that number into the RF array, and finally get
-                                            // the tag from the 2nd row. 
+            RF[stoi((*it)->dest_o)][1] = 0; 
         }
 
         (*it)->state = WB;
@@ -162,8 +169,6 @@ void Execute() {
 
 // Send to functional units (AKA the execute queue) if required registers are avaliable. Also check to see if the required sources registers are being used.
 void Issue() {
-    int count = 0;
-    int debug = 0;
     if (issue.empty()) return;
 
     std::sort(issue.begin(), issue.end(), [](instruction *a, instruction *b) {  // Sort by tag, ascending order.
@@ -171,13 +176,12 @@ void Issue() {
     });
     
     for (auto it = issue.begin(); it != issue.end();) {
-
-        /*
-        cout << "Current Instruction:" << endl;
-        cout << (*it)->address << " " << (*it)->dest_o << " " << (*it)->src1_o <<  " " << (*it)-> src2_o << " " << endl;
-        cout << RF[stoi((*it)->dest_o)][1] << " " << RF[stoi((*it)->src1_o)][1] << " " << RF[stoi((*it)->src2_o)][1] <<  " " << endl;
-        cout << endl;
-        */
+        // If we find that any of the instruction's operands (dest, src1, src2) are being used, with delay issuing that instruction.
+        // Also, again, the usage of stoi() and <reg>_o is to just get an index for the register file. 
+        if ((*it)->dest_o != "-1" && RF[stoi((*it)->dest_o)][1] == 1) {
+            it++;
+            continue;
+        }
 
         if ((*it)->src1_o != "-1" && RF[stoi((*it)->src1_o)][1] == 1) { // Instructions can have two source registers. If one of them is marked with -1, it's not using
                                                                         // another source register. Otherwise we check to see if the source reg is being used in the RF.
@@ -190,35 +194,34 @@ void Issue() {
             continue;
         }
 
+        if ((*it)->dest_o != "-1") {
+            RF[stoi((*it)->dest_o)][1] = 1; // 1 means not ready; register is being used. // went up ^ there where special flag line is.
+        }
+
         (*it)->state = EX;
+
         execute.push_back(*it);
         it = issue.erase(it);
-        count++;
     }
 }
 
-// We rename the instructions here.
+// We rename the instructions here, and push to the  queue. 
 void Dispatch() {
     int count = 0;
     if (!dispatch.empty() && issue.size() < N) {
-
         for (auto it = dispatch.begin(); it != dispatch.end();) {     
-            //cout << "count: " << count << endl;
+            //Since the issue queue essentially contains only N instructions, we push only that amount.
             if (issue.size() >= N) {break;}
-
-            if ((*it)->dest_o != "-1") {
-                RF[stoi((*it)->dest_o)][1] = 1; // 1 means not ready; register is being used.
-            }
-
             RenameOps((*it));
             (*it)->state = IS;
-
+            
             issue.push_back(*it);
             it = dispatch.erase(it);  
             count++;
         }
     }
 
+    // We only set N instructions at a time (dispatch has a size of 2N) to the ID state. This simulates the 1 cycle latency between IF -> ID.
     int i = 0;
     for (instruction *ins : dispatch) {
         if (i >= N) {  
@@ -227,12 +230,11 @@ void Dispatch() {
         (ins)->state = ID;
         i++;
     }
-    //cout << "hit" << endl;
 }
 
-// Again, if any one of the instruction's register is marked as -1, it doesn't have that register. 
+// Where the actual renaming takes place.
+// If any one of the instruction's register is marked as -1, it doesn't have that register, and so there's nothing to rename.
 void RenameOps(instruction *dispatched) {
-
     if (dispatched->dest != "-1") {
         dispatched->dest = to_string(RF[stoi(dispatched->dest_o)][0]);
     }
@@ -247,7 +249,7 @@ void RenameOps(instruction *dispatched) {
 
 // Get instructions.
 void Fetch() {
-    // For reference
+    // For reference:
     // <PC> <operation type> <dest reg #> <src1 reg #> <src2 reg #>
     int address;
     int operation;
@@ -257,7 +259,7 @@ void Fetch() {
     string src2;
     string line;
 
-    while (dispatch.size() < 2*N) { // Check if the dispatch queue is full. If yes, we skip for the time-being.
+    while (dispatch.size() < 2*N) { // Check if the dispatch queue is full. If yes, we skip fetching for the time-being.
         getline(file, line);
         if (line == "") { // The trace file has a weird empty line at the end which throws off the entire code, so I made sure to check for empty lines.
             file.close();
@@ -276,7 +278,7 @@ void Fetch() {
         ss >> src1;
         ss >> src2;
         instruction *ins = new instruction(address, operation, dest, src1, src2);
-        dispatch.push_back(ins); // Add to dispatch queue.
+        dispatch.push_back(ins);    // Add to dispatch queue.
         fakeROB.push_back(ins);     // Add to fakeROB
         if (dest != "-1") {
             RF[stoi(dest)][0] = tag_counter;    // Here we assign a tag to the register in the RF.
@@ -294,16 +296,16 @@ bool Advance_Cycle() {
     }
 }
 
-//Checks if empty. It's fine.
+//Checks if fakeROB empty. This means there are no longer any instructons left in the pipeline!
 bool isEmpty() {
-    /*if (cycle < 3) {
-        return false;
-    }*/
-        
-    if (dispatch.size() != 0) return false;
-    if (issue.size() != 0) return false;
-    if (execute.size() != 0) return false;
     if (fakeROB.size() != 0) return false;
     
     return true;
 }
+
+/* Some handy debug code
+cout << "<name of function this code is in>:" << endl;
+cout << (*it)->address << " " << (*it)->dest_o << " " << (*it)->src1_o <<  " " << (*it)-> src2_o << " " << (*it)->state << endl;
+cout << RF[stoi((*it)->dest_o)][1] << " " << RF[stoi((*it)->src1_o)][1] << " " << RF[stoi((*it)->src2_o)][1] <<  " " << endl;
+cout << endl;
+*/
